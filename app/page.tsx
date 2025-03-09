b32f593d4caded3c781575e504ed480f1225e399
Stepan Potiienko <kotnexjieb@protonmail.com>
2025-03-09 07:31:41 +0200

    add new habit form and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import './styles/page.css'
+
 const AddHabitForm = () => {
   function AddNewHabit() {
     // TODO
@@ -7,10 +9,10 @@ const AddHabitForm = () => {
   }
 
   return (
-    <div className="flex flex-col gap-3 w-1/4 text-center bg-amber-300">
+    <div className="flex flex-col gap-3 w-1/4 text-center bg-gradient-to-br from-yellow-400 to-red-500 rounded-4xl p-3" id="new-habit-form">
       <label htmlFor="add-habit">Add new habit:</label>
       <input id="add-habit" placeholder="Enter the name of the habit: "/>
-      <button type="submit" value={"Submit"} onSubmit={() => AddNewHabit()} className="cursor-pointer">Submit</button>
+      <button type="submit" value={"Submit"} onSubmit={() => AddNewHabit()} className="cursor-pointer hover:underline">Submit</button>
     </div>
   )
 }
@@ -18,10 +20,12 @@ const AddHabitForm = () => {
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-2xl">Sprinter</h1>
-      <p>Habit tracker made easy!</p>
-      <AddHabitForm />
+    <div className='w-[100vw] h-[80vh] flex justify-center items-center flex-col'>
+      <h1 className="text-4xl" style={{textShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)"}}>Sprinter</h1>
+      <h2 className='text-xl'>Habit tracker made easy!</h2>
+      <div className='w-[100vw] h-[30vh] flex justify-center align-middle items-center'>
+        <AddHabitForm />
+      </div>
     </div>
   );
 }
